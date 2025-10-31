a03e717a5baa9a7f89730b8d27d986d3e1971bd0
Leo Willems <leo@lewi.nl>
2025-10-31 11:08:55 +0100

    adding sources

--- cmd/collectfwdata/main.go
+++ cmd/collectfwdata/main.go
@@ -1,8 +1,20 @@
 package main
 
-import (
-    _ "github.com/LeoWillems2/checknet/pkg/sharedlib"
-)
+/*
+	collect UFW rules / Interfaces / Listeners for access
+		- not for routing etc.
+		- on interface does not support in or out
+*/
 
 func main() {
+	GetInterfaces(true)
+	//ProcessUFW(ReadUFWFile("ufw2.txt"))
+	//ProcessListeners(ReadListenerFile("ss2.txt"))
+
+	//for _, f := range []string{"nmap-internet2.txt"} {
+		//ProcessNMAP(ReadNMAPFile(f), f)
+	//}
+
+	ProcessUFW(ReadUFWProc())
+	ProcessListeners(ReadListenerProc())
 }
